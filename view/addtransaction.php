e5972350c8146687f256412e3ab8ad89842e254e
Victor <victor.inf2011@hotmail.com>
2020-01-08 17:45:18 -0300

    mais algumas implementacoes visuais

--- view/addtransaction.php
+++ view/addtransaction.php
@@ -4,19 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Add</title>
+    <link rel="stylesheet" href="styles/main.css">
 </head>
 <body>
-    <form action="../controller/addtransaction.php" method="post">
-        <label>Description: <input type="text" name="description" id="description" placeholder="Grocery Shopping"></label>
-        <p>Type: 
-            <input type="radio" name="type" value="expense" checked>Expense 
-            <input type="radio" name="type" value="recipe">Recipe
-        </p>
-        <label>Value: $<input type="number" name="price" id="price" placeholder="100.00" step="0.01"></label>
-        <label>Classification: <input type="search" name="classification" id="classification" placeholder="Market"></label>
-        <label>Date: <input type="date" name="date" id="date" value='<?php echo date("Y-m-d"); ?>'></label>
-        <button type="submit">Add</button>
-    </form>    
+    <div id="content">
+        <form action="../controller/addtransaction.php" method="post">
+            <div class="input"><label><p>Description: </p><input type="text" name="description" id="description" placeholder="Grocery Shopping"></label></div>
+            <div class="radio">Type: 
+                <input type="radio" name="type" value="expense" checked>Expense 
+                <input type="radio" name="type" value="recipe">Recipe
+            </div>
+            <div class="input"><label><p>Value: $</p><input type="number" name="price" id="price" placeholder="100.00" step="0.01"></label></div>
+            <div class="input"><label><p>Classification: </p><input type="search" name="classification" id="classification" placeholder="Market"></label></div>
+            <div class="input"><label><p>Date: </p><input type="date" name="date" id="date" value='<?php echo date("Y-m-d"); ?>'></label></div>
+            <button type="submit">Add</button>
+            <a href="home.php">Back</a>
+        </form>    
+    </div>
 </body>
 </html>
--- view/edittransaction.php
+++ view/edittransaction.php
@@ -8,8 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edit</title>
+    <link rel="stylesheet" href="styles/main.css">
 </head>
 <body>
-
+<a href="../home.php">Back</a>
 </body>
 </html>
